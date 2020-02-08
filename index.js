2eca695b2a93f3719e3d7544938ddce401fccb7e
kourosh g <kourosh.goodarzi@gmail.com>
2020-02-08 01:26:11 -0500

    Done calculation without final result

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ function calculate() {
   var inputElements = document.getElementsByClassName("selected_box");
   for (var i = 0; inputElements[i]; ++i) {
     if (inputElements[i].checked) {
-        checkedValue += parseFloat(inputElements[i].value); 
+      checkedValue += parseFloat(inputElements[i].value);
     }
   }
-    document.getElementById('selected_price').innerHTML = checkedValue
 
-    
+  /* Value before tax */
+  document.getElementById("selected_price").innerHTML = checkedValue;
+  /* value of the tax */
+  var taxAmount = parseFloat((checkedValue * 15) / 100);
+  var rounded_number = taxAmount.toFixed(2);
+  document.getElementById("tax_rate").innerHTML = rounded_number;
+  /* total cost */
+  var totalCost = checkedValue + rounded_number;
+  //document.getElementById("full_price").innerHTML = totalCost;
+  console.log(totalCost);
 }
