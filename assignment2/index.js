410fc2f2cc4cb1138bd17bf2e46be98fea377cda
kourosh g <kourosh.goodarzi@gmail.com>
2020-03-02 23:14:41 -0500

    better

--- assignment2/index.js
+++ assignment2/index.js
@@ -1,9 +1,11 @@
 /* mouse over section */
 var defaultImg;
-$('.coffee_pics').on('mouseenter', function (e) {
-  defaultImg = $(e.currentTarget).attr('src');
-  $(e.currentTarget).attr('src', $(this).data('nextimage'));
-}).on('mouseleave', function (e) {
-  $(e.currentTarget).attr('src', defaultImg);
-}); 
+$(".coffee_pics")
+  .on("mouseenter", function(e) {
+    defaultImg = $(e.currentTarget).attr("src");
+    $(e.currentTarget).attr("src", $(this).data("nextimage"));
+  })
+  .on("mouseleave", function(e) {
+    $(e.currentTarget).attr("src", defaultImg);
+  });
 /* End of mouse over section */
