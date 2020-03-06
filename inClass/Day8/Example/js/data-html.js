b5066c47b548efdcf566efc03f57ca99e93ba375
Koorosh Goodarzi <k_goodar@live.concordia.ca>
2020-03-05 21:27:10 -0500

    added class

---
+++ inClass/Day8/Example/js/data-html.js
@@ -0,0 +1,13 @@
+
+
+var xhr = new XMLHttpRequest();                 // Create XMLHttpRequest object
+
+xhr.onload = function() {                       // When response has loaded
+  // The following conditional check will not work locally - only on a server
+  if(xhr.status === 200) {                       // If server status was ok
+    document.getElementById('content').innerHTML = xhr.responseText; // Update
+  }
+};
+
+xhr.open('GET', 'data/data.html', true);        // Prepare the request
+xhr.send();                                 // Send the request
