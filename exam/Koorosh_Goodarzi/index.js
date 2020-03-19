35a0c29d7969c3053f194719472633e5d46e0313
kourosh g <kourosh.goodarzi@gmail.com>
2020-03-19 19:17:57 -0400

    it's ok

--- exam/Koorosh_Goodarzi/index.js
+++ exam/Koorosh_Goodarzi/index.js
@@ -1,12 +1,14 @@
+/* On load seation */
 $(document).ready(function() {
   $("#page p").text("Koorosh Goodarzi");
 });
 
+/* adding the movie section */
 function addMovie() {
   var movieName = $("#itemDescription").val();
   if (movieName) {
     var beforeLast = $("li:last-child");
-    beforeLast.after('<li class="the_item"></li>');
+    beforeLast.after("<li></li>");
     var last = $("li:last-child");
     last.text(movieName);
     $("#itemDescription").val("");
@@ -15,4 +17,25 @@ function addMovie() {
   }
 }
 
+/* mouse over on the title section */
+function mouseEnter(e) {
+  $(e.currentTarget)
+    .css("padding-left", "80px")
+    .css("font-size", "+=1em");
+}
+
+function mouseLeft(e) {
+  $(e.currentTarget)
+    .css("padding-left", "")
+    .css("font-size", "");
+}
+/* removing the item section */
+function removeElement(e) {
+  $(e.currentTarget).remove();
+}
+
+/* Event listiners */
+$("li").on("click", removeElement);
+$("li").on("mouseenter", mouseEnter);
+$("li").on("mouseleave", mouseLeft);
 $("input:eq(1)").on("click", addMovie);
